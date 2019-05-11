9f6c3964d91bbebe1aa979a41025714dd5e849b1
Ellyn Anderson <eka249@nyu.edu>
2019-05-11 21:43:00 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars= ['snickers','hundred grand', 'kitkat', 'skittles'];
+
+var a = ['one', 'two']
+var b= 'three'
+
+function addElementToBeginningOfArray(a, b){
+  return [... b, a];
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b){
+  return a.unshift(b);
+}
+
+function addElementToEndOfArray(a, b){
+  return [a, ...b];
+}
+
+function destructivelyAddElementToEndOfArray(a,b){
+  return a.push(b);
+}
+
+function accessElementInArray(a,b){
+  return a[b];
+}
